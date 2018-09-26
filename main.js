3bc4bd164acaa36e02be0bfc0102d06237e61354
geckohack389 <xberic45@hotmail.com>
2018-09-25 17:16:40 -0700

    Made it purty

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var Greeter = /** @class */ (function () {
     }
     Greeter.prototype.showGreeting = function () {
         var banner = document.getElementById('banner');
-        banner.innerHTML = 'Hello ' + this.first_name + ' ' + this.last_name + '.';
+        banner.innerHTML = 'Hello ' + this.first_name + ' ' + this.last_name + ', I take it you are having a good day' + '?';
     };
     Greeter.prototype.hideGreeting = function () {
         var banner = document.getElementById('banner');
@@ -14,3 +14,33 @@ var Greeter = /** @class */ (function () {
     return Greeter;
 }());
 var greeter = new Greeter('Eric', 'Rodriguez');
+var Troll = /** @class */ (function () {
+    function Troll(message) {
+        this.message = message;
+    }
+    Troll.prototype.showMessage = function () {
+        var rando = document.getElementById('rando');
+        rando.innerHTML = 'The message you looking for is elsewhere' + "," + ' ' + this.message + ".";
+    };
+    Troll.prototype.hideMessage = function () {
+        var rando = document.getElementById('rando');
+        rando.innerHTML = 'r3c2';
+    };
+    return Troll;
+}());
+var trolli = new Troll('Look above dummy');
+var notHere = /** @class */ (function () {
+    function notHere(scold) {
+        this.scold = scold;
+    }
+    notHere.prototype.showTease = function () {
+        var haha = document.getElementById('haha');
+        haha.innerHTML = 'Not here my dude.' + ' ' + this.scold + '.';
+    };
+    notHere.prototype.hideTease = function () {
+        var haha = document.getElementById('haha');
+        haha.innerHTML = 'r2c3';
+    };
+    return notHere;
+}());
+var teaser = new notHere('Getting closer.');
--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ class Greeter {
   showGreeting() {
    
     const banner = document.getElementById('banner');
-    banner.innerHTML = 'Hello ' + this.first_name + ' ' + this.last_name + '.';
+    banner.innerHTML = 'Hello ' + this.first_name + ' ' + this.last_name + ', I take it you are having a good day' + '?';
 
   }
   hideGreeting() {
@@ -17,10 +17,39 @@ class Greeter {
 
 
 }
-
-
-
-
 const greeter = new Greeter ('Eric', 'Rodriguez');
 
 
+class Troll {
+  constructor(private message: string) {
+
+  }
+  showMessage() {
+    const rando = document.getElementById('rando');
+    rando.innerHTML = 'The message you looking for is elsewhere' + "," + ' ' + this.message + "."
+  }
+
+  hideMessage() {
+      const rando = document.getElementById('rando');
+      rando.innerHTML = 'r3c2';
+    }
+
+
+  }
+
+const trolli = new Troll ('Look above dummy');
+
+class notHere {
+  constructor(private scold: string) {
+
+  }
+  showTease() {
+    const haha = document.getElementById('haha');
+    haha.innerHTML = 'Not here my dude.' + ' ' + this.scold + '.'
+  }
+  hideTease() {
+    const haha = document.getElementById('haha');
+    haha.innerHTML = 'r2c3';
+  }
+}
+const teaser = new notHere ('Getting closer.')
